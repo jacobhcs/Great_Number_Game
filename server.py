da3f13702dd4ce48b770acf7a6c486171f11c5b5
jacobhcs <jaharr1999@gmail.com>
2024-01-12 21:14:16 -0500

    changes

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ def generate_random_number():
 
 @app.route('/')
 def index():
+  session.clear()
   generate_random_number()
   print(session['random_num'])
   return render_template('index.html')
@@ -22,13 +23,23 @@ def guessgoal():
 @app.route('/show')
 def showguess():
   print(session['guess'])
-  if int(session['guess']) < session['random_num']:
+  if 'guess' not in session:
+        return redirect('/')
+  elif int(session['guess']) < session['random_num']:
      results = 'TO LOW'
+     session['color'] = 'bg-danger'
   elif int(session['guess']) > session['random_num']:
      results = "TOO HIGH"
+     session['color'] = 'bg-danger'
   else:
      results = "YOU GOT IT RIGHT!"
+     session['color'] = 'bg-success'
+     session['reset'] = True
   return render_template('guess.html', randomguess=session['guess'], results=results)
+
+@app.route('/reset', methods=['POST'])
+def reset():
+   return redirect('/')
 
 if __name__=='__main__':
   app.run(debug=True)
